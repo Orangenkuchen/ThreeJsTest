743f3d9204a598fa596ceb31e1b7231acb8ec499
Dominik Köth <dominik.koeth@gmx.de>
2024-04-13 20:57:19 +0200

    Vite-Config angepasst

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [],
-   base: '/ThreeJsTest/'
+   base: '/ThreeJsTest/',
+   build: {
+      sourcemap: true,
+      minify: false
+   },
 })
